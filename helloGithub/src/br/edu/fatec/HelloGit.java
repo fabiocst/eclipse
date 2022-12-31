94a90029083e21d73a39094e6f07e6792b307cd3
Fabio_cst <94999469+fabiocst@users.noreply.github.com>
2022-12-31 02:07:12 -0300

    Adicionei arquivos via upload

---
+++ helloGithub/src/br/edu/fatec/HelloGit.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package br.edu.fatec;
+
+/**
+ * @author fabio
+ * Exemplo de integração do Eclipse com o Github
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello GIT");
+		// TODO Auto-generated method stub
+
+	}
+
+}
